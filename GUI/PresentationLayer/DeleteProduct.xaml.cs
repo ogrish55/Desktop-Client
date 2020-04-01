a35ee0254ed9fffa8bc452e614c7a390b9633d30
Dan Lund Pedersen <danlund1994@gmail.com>
2020-04-01 11:43:20 +0200

    Tilføjet delete product

---
+++ GUI/PresentationLayer/DeleteProduct.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI.PresentationLayer
+{
+    /// <summary>
+    /// Interaction logic for DeleteProduct.xaml
+    /// </summary>
+    public partial class DeleteProduct : Window
+    {
+        public DeleteProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Hide();
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // indsæt heropeskf
+        }
+
+
+    }
+}
