9a7dd3a05bad41974c7f189034d7626b833afe82
Kristoffer K <Kristoffer92k@hotmail.com>
2020-04-07 11:09:14 +0200

    Opretter OrderController klasse og implemeneteret metoder hertil.

---
+++ GUI/ControlLayer/OrderController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GUI.ModelLayer;
+using GUI.Utilities;
+
+namespace GUI.ControlLayer
+{
+    class OrderController
+    {
+        public enum EnumStatus{
+            All,
+            Active,
+            Cancelled
+            };
+
+        public IEnumerable<Order> GetOrders(EnumStatus status)
+        {
+            OrderService os = new OrderService();
+            List<Order> listToReturn = new List<Order>();
+            if(status == EnumStatus.All)
+            {
+                foreach (var item in os.GetAllOrders())
+                {
+                    listToReturn.Add(new ConvertDataModel().ConvertFromServiceOrder(item));
+                }
+            }
+
+            if(status == EnumStatus.Cancelled)
+            {
+                foreach (var item in os.GetCancelledOrders())
+                {
+                    listToReturn.Add(new ConvertDataModel().ConvertFromServiceOrder(item));
+                }
+            }
+
+            if(status == EnumStatus.Active)
+            {
+                foreach (var item in os.GetActiveOrders())
+                {
+                    listToReturn.Add(new ConvertDataModel().ConvertFromServiceOrder(item));
+                }
+            }
+
+            return listToReturn;
+        }
+
+        public void CancelOrder(Order order)
+        {
+            new OrderService().UpdateOrder(new ConvertDataModel().ConvertToServiceOrder(order));
+        }
+    }
+}
