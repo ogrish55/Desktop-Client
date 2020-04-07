7f75ed8cb5740411a49533127805a7d14b865256
Kristoffer K <Kristoffer92k@hotmail.com>
2020-04-07 10:50:59 +0200

    Oprettet Discount klasse, Oprettet Enum for Orderstatus og PaymentMethods. Oprettet order klasse.

---
+++ GUI/ModelLayer/Discount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ModelLayer
+{
+   public class Discount
+    {
+        public int discountId { get; set; }
+        public string discountCode { get; set; }
+
+        public int Value { get; set; }
+    }
+}
---
+++ GUI/ModelLayer/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ModelLayer
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public decimal FinalPrice { get; set; }
+
+        public EnumOrderStatus Status { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Discount CurrentDiscount { get; set; }
+
+        public EnumPaymentMethods PaymentMethod { get; set; }
+
+        public Order()
+        {
+            Date = DateTime.Now;
+            Status = EnumOrderStatus.Active;
+        }
+    }
+}
