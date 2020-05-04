dfe27fb3e8c4bbb2b62f565d8bc83071b312c4c5
Kristoffer K <Kristoffer92k@hotmail.com>
2020-05-04 09:40:55 +0200

    Foretaget en masse ændringer i en masse klasser

--- GUI/ModelLayer/Product.cs
+++ GUI/ModelLayer/Product.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace GUI.ModelLayer
 {
     public class Product
@@ -12,7 +7,6 @@ namespace GUI.ModelLayer
         public decimal Price { get; set; }
         public string Description { get; set; }
         public int ProductId { get; set; }
-
         public int AmountOnStock { get; set; }
         public string Brand { get; set; }
 
@@ -21,6 +15,4 @@ namespace GUI.ModelLayer
             return $"{Name} - {Price} - {ProductId} - {Brand}";
         }
     }
-
-
 }
