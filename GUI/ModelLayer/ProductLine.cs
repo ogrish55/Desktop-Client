36a81196c04894a02438a4e1ce12e480df543d00
Shurikanz <1078121@gmail.com>
2020-05-20 08:18:28 +0200

    Test

---
+++ GUI/ModelLayer/ProductLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ModelLayer
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int ProductId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price} - {ProductId}";
+        }
+    }
+
+
+}
